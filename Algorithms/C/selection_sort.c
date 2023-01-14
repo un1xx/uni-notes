6f8462014401f9e14906d6bc932dd67632fd84f7
un1xx <31860157+un1xx@users.noreply.github.com>
2023-01-14 14:50:38 +0530

    Create selection_sort.c

---
+++ Algorithms/C/selection_sort.c
@@ -0,0 +1,47 @@
+// slection sort in c
+
+// bubble sort in C
+// Time complexity = O(n^2)
+#include <stdio.h>
+
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void printarr(int arr[], int size){
+    int i=0;
+    printf("[ ");
+    for (i; i<size; i++)
+        printf("%d ", arr[i]);
+    printf("]\n");
+}
+
+void selection_sort(int arr[], int size){
+    int i, j, min;
+
+    for (i=0; i<size-1; i++){
+        min=i;
+        for (j = i+1; j<size-1; j++)
+            if (arr[j] < arr[min])
+                min =j;
+        swap(&arr[min], &arr[i]);
+    }
+}
+
+
+
+
+int main(){
+int arr[] = {1, 2, 5, 6, 0, 193, 0, 38, 333};
+int size = 9;
+
+printf("before sort -");
+printarr(arr, size);
+printf("after sort -");
+selection_sort(arr, size);
+printarr(arr, size);
+}
